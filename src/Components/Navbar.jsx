86c294b891b090f0f5470fc1715384a9c57f7fde
Kaveh Asgari <kaveh.nilkaas@gmail.com>
2024-07-29 21:03:33 +0330

    [app] : add mobile size MegaMenu - responsive

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -5,16 +5,17 @@ import {
 } from "../Helpers/navbarMenuItems";
 import MegaMenu from "./MegaMenu/MegaMenu";
 
-export default function Navbar() {
+export default function Navbar({}) {
 
   return (
-    <div className="flex flex-row-reverse justify-between items-center pl-2 pr-6 h-10 bg-secondary-content relative text-menuItem ">
-      <button className="relative button-effect hover:scale-105 z-10 transition-all duration-300 overflow-hidden flex justify-between items-center text-xs  rounded-md p-2 pb-3 pr-4 gap-1 text-white h-6 tracking-tight">
-        <LoginSvg className="w-3 mt-1" />
+    <div className="flex flex-row-reverse justify-between items-center pl-3 pr-3 lg:pr-6 h-13 bg-secondary-content relative text-menuItem ">
+
+      <button className="relative button-effect hover:scale-105 z-10 transition-all duration-300 overflow-hidden flex justify-between items-center rounded-[0.4rem] p-2 pr-5 lg:p4  gap-1 text-white h-8 lg:h-8">
+        <LoginSvg className="w-4 lg:w-4 " />
         ورود | ثبت نام
       </button>
 
-      <MegaMenu navbarMenu={navbarMenu} />
+      <MegaMenu navbarMenu={navbarMenu}/>
     </div>
   );
 }
